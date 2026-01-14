58c91ac3768a2de5074b7f2c8d71245a92173128
ShowerMan <158011368+ShowerMan@users.noreply.github.com>
2026-01-14 15:35:07 -0800

    Fix indentation in ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -55,7 +55,7 @@ local ThemeManager = {} do
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
 		['Default']       = { 1, { FontColor = "ffffff", MainColor = "1c1c1c", AccentColor = "a05ae6", BackgroundColor = "141414", OutlineColor = "323232" } },
-		['LinoriaV2']          = { 2, { FontColor = "e5e5e5", MainColor = "1e1e1e", AccentColor = "ffffff", BackgroundColor = "232323", OutlineColor = "141414" } }
+		['LinoriaV2']     = { 2, { FontColor = "e5e5e5", MainColor = "1e1e1e", AccentColor = "ffffff", BackgroundColor = "232323", OutlineColor = "141414" } },
 		['BBot']          = { 2, { FontColor = "ffffff", MainColor = "1e1e1e", AccentColor = "7e48a3", BackgroundColor = "232323", OutlineColor = "141414" } },
 		['Fatality']      = { 3, { FontColor = "ffffff", MainColor = "1e1842", AccentColor = "c50754", BackgroundColor = "191335", OutlineColor = "3c355d" } },
 		['Jester']        = { 4, { FontColor = "ffffff", MainColor = "242424", AccentColor = "db4467", BackgroundColor = "1c1c1c", OutlineColor = "373737" } },
